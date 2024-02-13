e5feac765af2acc01e8d13fb25ac22f2b0994529
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-02-13 18:11:25 +0530

    feat: delete board functionality

---
+++ src/app/(platform)/(dashboard)/board/[boardId]/_components/BoardOptions.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { deleteBoard } from "@/actions/deleteBoard";
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverClose,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useAction } from "@/hooks/useAction";
+import { MoreHorizontal, X } from "lucide-react";
+import { toast } from "sonner";
+
+interface BoardOptionsProps {
+  id: string;
+}
+
+const BoardOptions = ({ id }: BoardOptionsProps) => {
+  const { execute, isLoading } = useAction(deleteBoard, {
+    OnError: error => {
+      toast.error(error);
+    },
+  });
+
+  const onDelete = () => {
+    execute({ id });
+  };
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button className="h-auto w-auto p-2" variant="transparent">
+          <MoreHorizontal className="h-4 w-4"></MoreHorizontal>
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="px-0 pt-3 pb-3" side="bottom" align="start">
+        <div className="text-sm font-medium text-center text-neutral-700">
+          Board actions
+        </div>
+        <PopoverClose>
+          <Button
+            className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
+            variant="ghost"
+          >
+            <X className="h-4 w-4 text-red-700"></X>
+          </Button>
+        </PopoverClose>
+        <Button
+          variant="ghost"
+          onClick={onDelete}
+          disabled={isLoading}
+          className="rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm"
+        >
+          Delete this board
+        </Button>
+      </PopoverContent>
+    </Popover>
+  );
+};
+export default BoardOptions;
