039d6b7d5cab0b1afcee6fda4453bbaab9423a78
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-02-14 13:24:25 +0530

    feat: list title optimistic update

--- src/app/(platform)/(dashboard)/board/[boardId]/_components/ListHeader.tsx
+++ src/app/(platform)/(dashboard)/board/[boardId]/_components/ListHeader.tsx
@@ -1,16 +1,97 @@
 "use client";
 
+import { updateList } from "@/actions/updateList";
+import { FormInput } from "@/components/form/FormInput";
+import { useAction } from "@/hooks/useAction";
 import { List } from "@prisma/client";
+import { ElementRef, useRef, useState } from "react";
+import { toast } from "sonner";
+import { useEventListener } from "usehooks-ts";
 
 interface ListHeaderProps {
   data: List;
 }
 const ListHeader = ({ data }: ListHeaderProps) => {
+  const [title, setTitle] = useState(data.title);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const formRef = useRef<ElementRef<"form">>(null);
+  const inputRef = useRef<ElementRef<"input">>(null);
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+      inputRef.current?.select();
+    });
+  };
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const { execute } = useAction(updateList, {
+    onSuccess: data => {
+      toast.success(`Renamed to "${data.title}"`);
+      setTitle(data.title);
+      disableEditing();
+    },
+    OnError: error => {
+      toast.error(error);
+    },
+  });
+
+  const handleSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    const id = formData.get("id") as string;
+    const boardId = formData.get("boardId") as string;
+
+    if (title === data.title) {
+      return disableEditing();
+    }
+    execute({
+      title,
+      id,
+      boardId,
+    });
+  };
+
+  const onBlur = () => {
+    formRef.current?.requestSubmit();
+  };
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      formRef.current?.requestSubmit();
+    }
+  };
+  useEventListener("keydown", onKeyDown);
   return (
     <div className="px-2 text-sm font-semibold flex justify-between items-start gap-x-2">
-      <div className="w-full text-sm px-2 5 py-1 h-7 font-medium border-transparent">
-        {data.title}
-      </div>
+      {isEditing ? (
+        <form className="flex-1 px-[2px]" action={handleSubmit} ref={formRef}>
+          <input type="hidden" name="id" id="id" value={data.id} />
+          <input
+            type="hidden"
+            name="boardId"
+            id="boardId"
+            value={data.boardId}
+          />
+          <FormInput
+            ref={inputRef}
+            onBlur={onBlur}
+            id="title"
+            placeholder="Enter list title..."
+            defaultVal={title}
+            className="text-sm px-[7px] py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition truncate bg-transparent focus:bg-white"
+          ></FormInput>
+          <button type="submit" hidden></button>
+        </form>
+      ) : (
+        <div
+          onClick={enableEditing}
+          className="w-full text-sm px-2 5 py-1 h-7 font-medium border-transparent"
+        >
+          {title}
+        </div>
+      )}
     </div>
   );
 };
--- src/app/(platform)/(dashboard)/board/[boardId]/_components/ListItem.tsx
+++ src/app/(platform)/(dashboard)/board/[boardId]/_components/ListItem.tsx
@@ -8,7 +8,7 @@ interface ListItemProps {
 const ListItem = ({ data, index }: ListItemProps) => {
   return (
     <li className="shrink-0 h-full w-[272px] select-none">
-      <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
+      <div className="w-full rounded-md bg-[#f1f2f4] shadow-md p-2">
         <ListHeader data={data}></ListHeader>
       </div>
     </li>
