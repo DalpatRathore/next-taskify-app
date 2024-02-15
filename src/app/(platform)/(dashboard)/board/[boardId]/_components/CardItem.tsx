1a39352a217b4dd69f2fe9b86ac485861a42381a
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-02-15 13:06:38 +0530

    fix: card list role button

--- src/app/(platform)/(dashboard)/board/[boardId]/_components/CardItem.tsx
+++ src/app/(platform)/(dashboard)/board/[boardId]/_components/CardItem.tsx
@@ -8,7 +8,10 @@ interface CardItemProps {
 }
 const CardItem = ({ data, index }: CardItemProps) => {
   return (
-    <div className="truncate border-2 border-transparent hover:border-black oy-2 px-3 text-sm bg-white rounded-md shadow-sm">
+    <div
+      className="truncate border-2 border-transparent hover:border-black oy-2 px-3 text-sm bg-white rounded-md shadow-sm"
+      role="button"
+    >
       {data.title}
     </div>
   );
