1aa182fa17c3c712b85054c208fb8cc99cddc69d
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-02-17 22:59:13 +0530

    feat: full functionality of strip

--- src/app/(platform)/(dashboard)/organization/[organizationId]/_components/BoardList.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/_components/BoardList.tsx
@@ -4,6 +4,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { MAX_FREE_BOARDS } from "@/constants/boards";
 import { db } from "@/lib/db";
 import { getAvailableCount } from "@/lib/orgLimit";
+import { checkSubscription } from "@/lib/subscription";
 import { auth } from "@clerk/nextjs";
 import { HelpCircle, User2 } from "lucide-react";
 import Link from "next/link";
@@ -25,6 +26,8 @@ const BoardList = async () => {
   });
 
   const availableCount = await getAvailableCount();
+
+  const isPro = await checkSubscription();
 
   return (
     <div className="space-y-4">
@@ -52,9 +55,11 @@ const BoardList = async () => {
             role="button"
           >
             <p className="text-sm">Create new board</p>
-            <span className="text-xs">{`${
-              MAX_FREE_BOARDS - availableCount
-            } remaining`}</span>
+            <span className="text-xs">
+              {isPro
+                ? "Unlimited"
+                : `${MAX_FREE_BOARDS - availableCount} remaining`}
+            </span>
             <HintTooltip
               sideOffset={40}
               description="Free workspace can have up to 5 open boards. For unlimited boards upgrade this workspace"
